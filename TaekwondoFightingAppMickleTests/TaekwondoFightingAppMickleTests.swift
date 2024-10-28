d6bed39d8a79cc58ddcbeae1a4b8d99ebbe07b7f
Peter Mickle <peter.mickle26@d155.org>
2024-10-28 08:15:49 -0500

    Initial Commit

---
+++ TaekwondoFightingAppMickleTests/TaekwondoFightingAppMickleTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaekwondoFightingAppMickleTests.swift
+//  TaekwondoFightingAppMickleTests
+//
+//  Created by PETER MICKLE on 10/28/24.
+//
+
+import Testing
+@testable import TaekwondoFightingAppMickle
+
+struct TaekwondoFightingAppMickleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
